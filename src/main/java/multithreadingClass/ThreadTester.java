265a64a4a84ba7c3ba1d198a8ead1661a9850f07
ankit-at-github <ankit.kumar.pune@gmail.com>
2023-10-07 10:18:24 -0400

    Thread Joining Concept

--- src/main/java/multithreadingClass/ThreadTester.java
+++ src/main/java/multithreadingClass/ThreadTester.java
@@ -42,26 +42,41 @@ public class ThreadTester {
 //                System.out.println("Popped: "+stack.pop());
 //        }, "Popper").start();
 //
-//        System.out.println("main is exiting");
+
 
 //------------------------------------------------------------------------------------------------------------------
-    Thread thread3 = new Thread(() -> {
+//    Thread thread3 = new Thread(() -> {
+//        try {
+//            Thread.sleep(1);
+//            for(int i=10000; i>0; i--);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }, "States");
+//
+//    thread3.start();
+//
+//    while(true)
+//    {
+//        Thread.State state = thread3.getState();
+//        System.out.println(state);
+//        if(state == Thread.State.TERMINATED) break;
+//    }
+//
+//-----------------------Thread joining concept-------------------------------------------------------------------------------------------
+
+        Thread thread = new Thread(() -> {
+            System.out.println(Thread.currentThread());
+        }, "Our Thread");
+        thread.start();
+
         try {
-            Thread.sleep(1);
-            for(int i=10000; i>0; i--);
+            thread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }, "States");
 
-    thread3.start();
-
-    while(true)
-    {
-        Thread.State state = thread3.getState();
-        System.out.println(state);
-        if(state == Thread.State.TERMINATED) break;
-    }
+        System.out.println("main is exiting");
 
     }
 }
